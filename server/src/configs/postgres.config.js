a11557a97f9b921a2745c34aa37c16e56664e05c
Baybhin Gurung <baybhin@gmail.com>
2024-05-24 21:06:12 -0400

    Updated Postgresql config file, so now it can successfully connect to db

--- server/src/configs/postgres.config.js
+++ server/src/configs/postgres.config.js
@@ -3,7 +3,7 @@ const { Pool } = require('pg');
 
 const pool = new Pool({
     user: 'renderer',
-    host: 'tileserver',
+    host: 'maps-clone_tileserver_1',
     database: 'gis',
     password: 'renderer',
     port: 5432, // Port mapped to the PostgreSQL container
